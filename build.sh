f49b775f518b8b27fd9932d10d1f29adc6f2eb6a
tzmax <71716824+tzmax@users.noreply.github.com>
2023-02-14 19:29:34 +0800

    feat: submit build ci workflow

--- build.sh
+++ build.sh
@@ -1,30 +1,9 @@
 #!/usr/bin/env bash
-
-#  compilefromsource.sh
-#  V2RayX
-#
-#  Created by Cenmrev on 10/15/16.
-#  Copyright © 2016 Cenmrev. All rights reserved.
-
-# http://apple.stackexchange.com/questions/50844/how-to-move-files-to-trash-from-command-line
-function moveToTrash () {
-  local path
-  for path in "$@"; do
-    # ignore any arguments
-    if [[ "$path" = -* ]]; then :
-    else
-      # remove trailing slash
-      local mindtrailingslash=${path%/}
-      # remove preceding directory path
-      local dst=${mindtrailingslash##*/}
-      # append the time if necessary
-      while [ -e ~/.Trash/"$dst" ]; do
-        dst="`expr "$dst" : '\(.*\)\.[^.]*'` `date +%H-%M-%S`.`expr "$dst" : '.*\.\([^.]*\)'`"
-      done
-      mv "$path" ~/.Trash/"$dst"
-    fi
-  done
-}
+###
+ # @Author: Bin
+ # @Date: 2023-02-14
+ # @FilePath: /V2RayXS/build.sh
+### 
 
 VERSION=$(git describe --tags --always)
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
@@ -54,10 +33,6 @@ if [[ ! -f /Applications/Xcode.app/Contents/MacOS/Xcode ]]; then
     echo -e "${RED}Xcode is needed to build V2RayXS, Please install Xcode from App Store!${NORMAL}"
     echo -e "${RED}编译 V2RayXS 需要 Xcode.app，请从 App Store 里安装 Xcode.${NORMAL}"
 else
-    echo -e "${BOLD}-- Downloading source code --${NORMAL}"
-    echo -e "${BOLD}-- 正在下载源码 --${NORMAL}"
-    git clone --recursive https://github.com/tzmax/V2RayXS.git "V2RayXS${datetime}"
-    cd "V2RayXS${datetime}"
     echo -e "${BOLD}-- Start building V2RayXS --${NORMAL}"
     echo -e "${BOLD}-- 开始编译 V2RayXS --${NORMAL}"
     xcodebuild "${XCODEBUILD_ARGS[@]}"
@@ -65,10 +40,23 @@ else
         echo -e "${GREEN}-- Build succeeded --${NORMAL}"
         echo -e "${GREEN}-- 编译成功 --${NORMAL}"
         echo -e "${BOLD}V2RayXS.app: $(pwd)/build/Release/V2RayXS.app${NORMAL}"
+
+        echo -e "${BOLD}-- Start packing --${NORMAL}"
+        isbeta=$(git describe --abbrev=0 --tags | grep beta)
+        if [[ "$isbeta" != "" ]] 
+        then 
+            xcodebuild -project V2RayXS.xcodeproj -target V2RayXS -configuration Debug -s
+            cd build/Debug/
+        else
+            cd build/Release/
+        fi
+
+        zip -r V2RayXS.app.zip V2RayXS.app && mkdir -p ../out/ && rsync -a V2RayXS.app.zip "../out/V2RayXS_${useArch}.app.zip"
+        echo -e "${GREEN}-- Packaging succeeded --${NORMAL}"
+        cd ->/dev/null 2>&1
+
     else
         echo -e "${RED}-- Build failed --${NORMAL}"
         echo -e "${RED}-- 编译失败 --${NORMAL}"
     fi
 fi
-
-
